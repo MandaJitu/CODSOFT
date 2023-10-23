9267ff04c5a2860e6a07f231ac8283019ca2ebd7
Manda Jitu <mandajitu01@gmail.com>
2023-10-23 20:42:30 +0530

    Task completed

--- Number_game.java
+++ Number_game.java
@@ -4,12 +4,12 @@ public class Number_game{
     public static void main(String[] args){
         Random number = new Random();
         int Random_number = number.nextInt(100) + 1;
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         int attempts = 0;
         while(true){
             System.out.println("Enter a number between 1 to 100: ");
-            int Guess_number = scanner.nextInt();
+            int Guess_number = sc.nextInt();
             attempts = attempts+1;
         if(Random_number == Guess_number){ 
             System.out.println("Yes! Your guess is correct.");
@@ -23,7 +23,8 @@ public class Number_game{
             System.out.println("Ohh! Your guess is high.");
         }
        }
-        scanner.close();
+       sc.close();
+        
     }
     
 }
