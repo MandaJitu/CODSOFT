a1dc743e21f3aeef18b55eafbb841c1e81942d6c
Manda Jitu <mandajitu01@gmail.com>
2023-10-23 20:38:06 +0530

    Scanner class name changed

--- Number_game.java
+++ Number_game.java
@@ -4,12 +4,12 @@ public class Number_game{
     public static void main(String[] args){
         Random number = new Random();
         int Random_number = number.nextInt(100) + 1;
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         int attempts = 0;
         while(true){
             System.out.println("Enter a number between 1 to 100: ");
-            int Guess_number = sc.nextInt();
+            int Guess_number = scanner.nextInt();
             attempts = attempts+1;
         if(Random_number == Guess_number){ 
             System.out.println("Yes! Your guess is correct.");
@@ -23,7 +23,7 @@ public class Number_game{
             System.out.println("Ohh! Your guess is high.");
         }
        }
-        sc.close();
+        scanner.close();
     }
     
 }
